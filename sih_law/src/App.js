48dfaf6bc33862eed4a437bcd64ff3c226a7b65f
harsha <saiharshapingali@gmail.com>
2023-09-26 08:38:25 +0530

    done

---
+++ sih_law/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import Home from "./templates/HomeLanding";
+import Navigation from "./templates/Navigation";
+import GoogleMaps from "./templates/GoogleMaps";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Mediator from "./templates/Mediator";
+import ViewHome from "./templates/ViewHome";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/advocate" element={<Navigation />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/viewhome/:id" element={<ViewHome />} />
+        <Route path="/map" element={<GoogleMaps />} />
+        <Route path="/mediator" element={<Mediator />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
